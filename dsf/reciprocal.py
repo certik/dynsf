2d89644a012636bb877ad7e45645f3b56f12674a
Mattias Slabanja <slabanja@chalmers.se>
2012-02-20 15:12:00 +0100

    Shape up a few comments and variables names.

--- dsf/reciprocal.py
+++ dsf/reciprocal.py
@@ -22,6 +22,7 @@ __all__ = ['reciprocal']
 import sys
 from os.path import dirname, join
 import numpy as np
+from numpy import linalg, array, arange, require, nonzero, pi, sqrt, prod
 from ctypes import cdll, byref, c_int, c_float, POINTER
 
 
@@ -52,8 +53,8 @@ for t in "ds":
                                 ndp_c_1d_rw[t], ndp_c_2d_rw[t])
 
 def calc_rho_k(x, k, ftype='d'):
-    x = np.require(x, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
-    k = np.require(k, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
+    x = require(x, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
+    k = require(k, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
     _, Nx = x.shape
     _, Nk = k.shape
     rho_k = np.zeros((Nk,), dtype=np_c[ftype], order='F')
@@ -62,9 +63,9 @@ def calc_rho_k(x, k, ftype='d'):
 
 def calc_rho_j_k(x, v, k, ftype='d'):
     assert x.shape == v.shape
-    x = np.require(x, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
-    v = np.require(v, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
-    k = np.require(k, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
+    x = require(x, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
+    v = require(v, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
+    k = require(k, np_f[ftype], ['F_CONTIGUOUS', 'ALIGNED'])
     _, Nx = x.shape
     _, Nk = k.shape
     rho_k = np.zeros((Nk,), dtype=np_c[ftype], order='F')
@@ -82,14 +83,17 @@ class reciprocal:
 
 
         Optionally limit the set to approximately N_max points by
-        randomly removing points. The points are removed in such a way
-        that for k>k_prune, the points will be radially uniformely
-        distributed (the value of k_prune is calculated from k_max, N_max,
+        randomly removing points from a "fully populated grid".
+        The points are removed in such a way that for k > k_prune,
+        the points will be radially uniformely distributed
+        (the value of k_prune is calculated from k_max, N_max,
         and the shape of the box).
 
-        k_max should be the "physicist reciprocal length" (with 2*pi factor)
+        Variables named k_ (such as input argument k_max) are expected
+        to be "physicist reciprocal length" (i.e. _with_ 2*pi factor).
+        Variables named q_ are expected to be without the 2*pi factor.
 
-        dtype can be either 'd' or 's' (double or single precission)
+        ftype can be either 'd' or 's' (double or single precission)
         """
         assert(N_max == -1 or N_max > 1000)
 
@@ -98,63 +102,70 @@ class reciprocal:
 
         self.ftype = ftype
         npftype = np_f[ftype]
-        self.A = np.require(box.copy(), npftype)
+        self.A = require(box.copy(), npftype)
         # B is the "crystallographic" reciprocal vectors
-        self.B = np.linalg.inv(self.A.transpose())
+        self.B = linalg.inv(self.A.transpose())
 
         self.debug = debug
 
-        q_max = k_max/(2.0*np.pi)
-        q_mins = np.array([np.linalg.norm(b) for b in self.B])
-        q_vol = np.prod(q_mins)
+        q_max = k_max/(2.0*pi)
+        q_mins = array([linalg.norm(b) for b in self.B])
+        q_vol = prod(q_mins)
         self.q_mins = q_mins
         self.q_vol = q_vol
 
-        if N_max == -1 or N_max > np.pi*q_max**3/(6*q_vol):
-            # Do not deselect k-points
+        if N_max == -1 or N_max > pi*q_max**3/(6*q_vol):
+            # Use all k-points, do not throw any away
             self.q_prune = None
         else:
             # Use Cardano's formula to find k_prune
             p = -3.0*q_max**2/4
-            q = 3.0*N_max*q_vol/np.pi - q_max**3/4
+            q = 3.0*N_max*q_vol/pi - q_max**3/4
             D = (p/3)**3 + (q/2)**2
             #assert D < 0.0
-            u = (-q/2+np.sqrt(D+0j))**(1.0/3)
-            v = (-q/2-np.sqrt(D+0j))**(1.0/3)
-            x = -(u+v)/2 - 1j*(u-v)*np.sqrt(3)/2
+            u = (-q/2+sqrt(D+0j))**(1.0/3)
+            v = (-q/2-sqrt(D+0j))**(1.0/3)
+            x = -(u+v)/2 - 1j*(u-v)*sqrt(3)/2
             self.q_prune = np.real(x) + q_max/2
 
-        b1, b2, b3 = [(2*np.pi)*x.reshape((3,1,1,1)) for x in self.B]
-        Nk1, Nk2, Nk3 = [np.ceil(q_max/dq) for dq in q_mins]
-        kvals = \
-            b1 * np.arange(Nk1, dtype=npftype).reshape((1,Nk1,1,1)) + \
-            b2 * np.arange(Nk2, dtype=npftype).reshape((1,1,Nk2,1)) + \
-            b3 * np.arange(Nk3, dtype=npftype).reshape((1,1,1,Nk3))
-        kvals = kvals.reshape((3, kvals.size/3))
-        qdist = np.sqrt(np.sum(kvals**2, axis=0))*(1.0/(2*np.pi))
-        I = np.nonzero(qdist<=q_max)[0]
-        I = I[qdist[I].argsort()]
-        qdist = qdist[I]
-        kvals = kvals[:,I]
-        if not self.q_prune is None:
-            N = len(qdist)
+        b1, b2, b3 = [(2*pi)*x.reshape((3,1,1,1)) for x in self.B]
+        N_k1, N_k2, N_k3 = [np.ceil(q_max/dq) for dq in q_mins]
+        k_points = \
+            b1 * arange(N_k1, dtype=npftype).reshape((1,N_k1,1,   1)) + \
+            b2 * arange(N_k2, dtype=npftype).reshape((1,1,   N_k2,1)) + \
+            b3 * arange(N_k3, dtype=npftype).reshape((1,1,   1,   N_k3))
+        k_points = k_points.reshape((3, k_points.size/3))
+        q_distance = sqrt(np.sum(k_points**2, axis=0))*(1.0/(2*pi))
+
+        I, = nonzero(q_distance <= q_max)
+        I = I[q_distance[I].argsort()]
+        q_distance = q_distance[I]
+        k_points = k_points[:,I]    # All k_points < k_max, sorted by length
+
+        if self.q_prune is not None:
+            N = len(q_distance)
+
+            # Keep point with probability min(1, (q_prune/|q|)^2) ->
+            # aim for an equal number of points per equally thick "onion peel"
+            # to even the statistict per radial unit.
             p = np.ones(N)
-            # N(k) = a k^3
-            # N'(k) = 3a k^2
-            p[1:] = (self.q_prune/qdist[1:])**2
-            I = np.nonzero(p > np.random.rand(N))[0]
-            qdist = qdist[I]
-            kvals = kvals[:,I]
-        self.kvals = kvals
-        self.qdist = qdist
-        N = len(qdist)
-        self.kdist = 2.0*np.pi*qdist
-        self.kdirect = kvals / (self.kdist.reshape((1,N)))
+            p[1:] = (self.q_prune/q_distance[1:])**2
+
+            I, = nonzero(p > np.random.rand(N))
+            q_distance = q_distance[I]
+            k_points = k_points[:,I]
+
+        self.k_points = k_points
+        self.q_distance = q_distance
+        self.k_distance = 2.0*pi*q_distance
+        N = len(q_distance)
+        self.k_direct = k_points.copy()
+        self.k_direct[:,1:] /= self.k_distance[1:].reshape((1,N))
 
     def process_frame(self, frame):
         """Add k-space density to frame
 
-        Calculate the density in k-space for dynsf-style trajectory frame.
+        Calculate the density in k-space for a dynsf-style trajectory frame.
         """
         if self.debug:
             sys.stdout.write("processing frame %i\r" % frame['step'])
@@ -162,17 +173,16 @@ class reciprocal:
 
         frame = frame.copy()
         if 'vs' in frame:
-            rho_ks, j_ks = zip(*[calc_rho_j_k(x, v, self.kvals,
+            rho_ks, j_ks = zip(*[calc_rho_j_k(x, v, self.k_points,
                                               ftype=self.ftype)
                                  for x,v in zip(frame['xs'],frame['vs'])])
-            jz_ks = [np.sum(j*self.kdirect, axis=0) for j in j_ks]
+            jz_ks = [np.sum(j*self.k_direct, axis=0) for j in j_ks]
             frame['j_ks'] = j_ks
             frame['jz_ks'] = jz_ks
-            frame['jpar_ks'] = [j-(jz*self.kdirect) for j,jz in zip(j_ks, jz_ks)]
+            frame['jpar_ks'] = [j-(jz*self.k_direct) for j,jz in zip(j_ks, jz_ks)]
             frame['rho_ks'] = rho_ks
         else:
-            frame['rho_ks'] = [calc_rho_k(x, self.kvals,
-                                          ftype=self.ftype)
+            frame['rho_ks'] = [calc_rho_k(x, self.k_points, ftype=self.ftype)
                                for x in frame['xs']]
         return frame
 
