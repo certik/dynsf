e82a42f0512760e4061508ad818a2dccb23e919a
Mattias Slabanja <slabanja@chalmers.se>
2012-06-04 09:55:16 +0200

    Add --dt option for overriding delta-timestep value.

--- build_config.py
+++ build_config.py
@@ -1,5 +1,5 @@
 
-extra_compile_args = ['-fopenmp']
+extra_compile_args = ['-fopenmp', '-O3', '-std=c99']
 extra_link_args = ['-fopenmp']
 
 # Let local_compiler be None in order to use the default compiler
@@ -8,7 +8,17 @@ local_compiler = None
 local_linker = local_compiler
 
 
-# # Example: Use icc instead of the default (gcc) compiler
+# Example: Explicitly use gcc
+#
+local_compiler = 'gcc'
+extra_compile_args = ['-fPIC', '-fopenmp', '-O3', '-march=nativ', '-std=c99']
+
+local_linker = local_compiler
+local_link_shared = ['-shared']
+extra_link_args = ['-fopenmp']
+
+
+# # Example: Use icc instead of the default compiler
 # #
 # local_compiler = 'icc'
 # extra_compile_args = ['-openmp', '-xHOST', '-O3', '-fno-alias', '-fPIC']
@@ -18,7 +28,7 @@ local_linker = local_compiler
 # extra_link_args = ['-openmp']
 
 
-# # Example: Use pgcc instead of the default (gcc) compiler
+# # Example: Use pgcc instead of the default compiler
 # #
 # local_compiler = 'pgcc'
 # extra_compile_args = ['-mp=numa', '-O4', '-Msafeptr', '-fPIC']
