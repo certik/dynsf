58eb5da920458f178cc5cf0e5fc7188597026d19
Mattias Slabanja <slabanja@chalmers.se>
2012-06-14 13:32:53 +0200

    Update build_config, add c99-option.

--- build_config.py
+++ build_config.py
@@ -8,20 +8,20 @@ local_compiler = None
 local_linker = local_compiler
 
 
-# Example: Explicitly use gcc
-#
-local_compiler = 'gcc'
-extra_compile_args = ['-fPIC', '-fopenmp', '-Ofast', '-march=native', '-std=c99']
+# # Example: Explicitly use gcc
+# #
+# local_compiler = 'gcc'
+# extra_compile_args = ['-fPIC', '-fopenmp', '-Ofast', '-march=native', '-std=c99']
 
-local_linker = local_compiler
-local_link_shared = ['-shared']
-extra_link_args = ['-fopenmp']
+# local_linker = local_compiler
+# local_link_shared = ['-shared']
+# extra_link_args = ['-fopenmp']
 
 
 # # Example: Use icc instead of the default compiler
 # #
 # local_compiler = 'icc'
-# extra_compile_args = ['-openmp', '-xHOST', '-O3', '-fno-alias', '-fPIC']
+# extra_compile_args = ['-openmp', '-xHOST', '-O3', '-fno-alias', '-fPIC', '-std=c99']
 
 # local_linker = local_compiler
 # local_link_shared = ['-shared']
@@ -31,7 +31,7 @@ extra_link_args = ['-fopenmp']
 # # Example: Use pgcc instead of the default compiler
 # #
 # local_compiler = 'pgcc'
-# extra_compile_args = ['-mp=numa', '-O4', '-Msafeptr', '-fPIC']
+# extra_compile_args = ['-mp=numa', '-O4', '-Msafeptr', '-fPIC', '-c99']
 
 # local_linker = local_compiler
 # local_link_shared = ['-shared']
